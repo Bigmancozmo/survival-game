8babeaaa039fa4e9b45ca4ebb42f48c018117432
Bigmancozmo <67850768+Bigmancozmo@users.noreply.github.com>
2021-10-24 16:45:20 -0400

    Add files via upload

--- main.lua
+++ main.lua
@@ -1,30 +1,88 @@
--- main.lua - basically stitch together all the scripts --
+-- main.lua - stitch together all the scripts to make a fun game --
 
-player = require("player")
-fps = require("ui/fps")
+require("player")
+require("ui/fps")
 require("libs/colorize")
-
-function getCenterPos(x,y,w,h)p={}p["x"]=x+(w/2)p["y"]=y+(h/2)return p end
+require("ui/button")
+require("libs/extensions")
 
 function love.load()
     -- Create variables
+    gameVersionn = "1"
+    gamestate = "menu"
     speed = 4
-    position = {}
+    plrPosition = {}
     winSize = {}
     plrSize = 40
+    local healthn = 100
+    local hasPlayed
+
+    if not love.filesystem.exists("game.sav") then
+        savename = "game.sav"
+        saveGame()
+        success = love.filesystem.write(savename, datacomp)
+    end
+    love.setDeprecationOutput(false)
+    savename = "game.sav"
+    chunk = love.filesystem.load(savename)
+    chunk()
 
     -- Set table values
     winSize["w"] = 800
     winSize["h"] = 600
-    position["x"] = winSize["w"] / 2 - plrSize / 2
-    position["y"] = winSize["h"] / 2 - plrSize / 2
+    plrPosition["x"] = winSize["w"] / 2 - plrSize / 2
+    plrPosition["y"] = winSize["h"] / 2 - plrSize / 2
 end
 
 function love.update(dt)
-    player.playerUpdate(position,speed,winSize,plrSize)
+    if gamestate == "play" then
+        player.playerUpdate(plrPosition,speed,winSize,plrSize)
+    end
+end
+
+function saveGame()
+    savename = "game.sav"
+    datacomp = "health="..health..";".."gameVersion="..gameVersionn..";"
+    success = love.filesystem.write(savename, datacomp)
+end
+
+function getHurt(dmg)
+    health = health - dmg;
 end
 
 function love.draw()
-    player.draw()
+    if gamestate == "play" or gamestate == "pause" then
+        love.graphics.setColor(rgba(255,0,0,1))
+        love.graphics.rectangle("fill",450,450,40,40)
+        player.draw(health)
+        love.graphics.setColor(hex("#fff"))
+        love.graphics.rectangle("fill",700,0,100,50)
+        love.graphics.setColor(hex("#000"))
+        love.graphics.print("Save",730,10,0,2)
+        if button.isMouseOver(700,0,100,50) then
+            love.graphics.setColor(hex("#bbb"))
+            if button.click() then
+                gamestate = "play"
+                saveGame();
+            end
+        end
+    end
+    if gamestate == "menu" then
+        if button.isMouseOver(300,380,200,60) then
+            love.graphics.setColor(hex("#bbb"))
+            if button.click() then
+                gamestate = "play"
+            end
+        else
+            love.graphics.setColor(rgba(255,255,255,1))
+        end
+        love.graphics.rectangle("fill",300,380,200,60)
+        local mid = extensions.getCenterPos(200,60,300,380)
+        love.graphics.setColor(0,0,0,1)
+        love.graphics.print("Play",mid["x"] + 10,mid["y"] + 140, 0, 3)
+        love.graphics.setColor(hex("#fff"))
+        love.graphics.print("*Please remember to save\n before quitting!\n I have not coded autosave\n yet.",300,200, 0, 2)
+    end
+    love.graphics.setColor(hex("#fff"))
     fps.draw()
 end
--- player.lua
+++ player.lua
@@ -1,6 +1,12 @@
+-- lol it's so hard to locate bugs in this code
+-- oh well at least good player functionality requires
+-- lots of code!
+
 player = {}
 
 require("libs/colorize")
+require("libs/extensions")
+local healthReqquire = require("ui/health")
 
 edge = false
 up = love.keyboard.isDown("up")
@@ -8,6 +14,9 @@ right = love.keyboard.isDown("right")
 down = love.keyboard.isDown("down")
 left = love.keyboard.isDown("left")
 moving = up or left or down or right
+eyeOffs = 0
+eyeOffsY = 0
+isCollidingWithAnObject = false
 
 function player.checkPlayerBounds(position,winSize,plrSize)
     u = false
@@ -66,23 +75,80 @@ function player.playerUpdate(position,speed,winSize,plrSize)
     if(love.keyboard.isDown("right")) then
         position["x"] = position["x"] + speed
     end
+    if right then
+        eyeOffs = 5
+    elseif left then
+        eyeOffs = -5
+    else
+        eyeOffs = 0
+    end
+    if up then
+        eyeOffsY = -5
+    elseif down then
+        eyeOffsY = 5
+    else
+        eyeOffsY = 0
+    end
+
     player.checkPlayerBounds(position,winSize,plrSize)
+    player.checkCollisions(position,plrSize)
 end
-function player.draw()
-    -- Some small data
+function player.checkCollisionAt(x,y,w,h,position,plrSize)
+    isCollidingWithAnObject = false
+    if up then 
+        position["y"] = position["y"] - 4
+        if extensions.isColliding(position["x"],position["y"],plrSize,plrSize,x,y,w,h) then
+            position["y"] = position["y"] + 4
+            isCollidingWithAnObject = true
+        end
+        position["y"] = position["y"] + 4
+    end
+    if down then 
+        position["y"] = position["y"] + 4
+        if extensions.isColliding(position["x"],position["y"],plrSize,plrSize,x,y,w,h) then
+            position["y"] = position["y"] - 4
+            isCollidingWithAnObject = true
+        end
+        position["y"] = position["y"] - 4
+    end
+    if left then 
+        position["x"] = position["x"] - 4
+        if extensions.isColliding(position["x"],position["y"],plrSize,plrSize,x,y,w,h) then
+            position["x"] = position["x"] + 4
+            isCollidingWithAnObject = true
+        end
+        position["x"] = position["x"] + 4
+    end
+    if right then 
+        position["x"] = position["x"] + 4
+        if extensions.isColliding(position["x"],position["y"],plrSize,plrSize,x,y,w,h) then
+            position["x"] = position["x"] - 4
+            isCollidingWithAnObject = true
+        end
+        position["x"] = position["x"] - 4
+    end
+end
+function player.checkCollisions(position,plrSize)
+    player.checkCollisionAt(450,450,40,40,position,plrSize);
+    if isCollidingWithAnObject then
+        health = health - 1
+    end
+end
+function player.draw(healthNum)
     eyeMargin = 8
 
     -- Basic box
     love.graphics.setColor(hex("#ffd88a")); -- Tan. Will later be the user's choice of color.
-    love.graphics.rectangle("fill",position["x"],position["y"],plrSize,plrSize);
+    love.graphics.rectangle("fill",plrPosition["x"],plrPosition["y"],plrSize,plrSize);
+    healthReqquire.draw(healthNum,plrPosition["x"] - 12.5,plrPosition["y"] - 15)
 
     -- Clothes
-    love.graphics.setColor(hex("#382500")); -- 
-    love.graphics.rectangle("fill",position["x"],position["y"] + (plrSize / 3) * 2,plrSize,plrSize / 3);
+    love.graphics.setColor(hex("#382500")); -- Brown
+    love.graphics.rectangle("fill",plrPosition["x"],plrPosition["y"] + (plrSize / 3) * 2,plrSize,plrSize / 3);
 
     -- Face
     love.graphics.setColor(hex("#000")); -- Black
-    love.graphics.rectangle("fill",position["x"] + eyeMargin,position["y"] + 10,plrSize / 6,plrSize / 6); -- Left eye
-    love.graphics.rectangle("fill",position["x"] + (plrSize - (eyeMargin * 2)),position["y"] + 10,plrSize / 6,plrSize / 6); -- Right eye
+    love.graphics.rectangle("fill",plrPosition["x"] + eyeMargin + eyeOffs,plrPosition["y"] + eyeOffsY + 10,plrSize / 6,plrSize / 6); -- Left eye
+    love.graphics.rectangle("fill",plrPosition["x"] + (plrSize - (eyeMargin * 2)) + eyeOffs,plrPosition["y"] + eyeOffsY + 10,plrSize / 6,plrSize / 6); -- Right eye
 end
 return player
